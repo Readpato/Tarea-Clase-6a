25e5291c7cb2c86867dacc610f1f4cdd6060b6ac
Readpato <87768087+Readpato@users.noreply.github.com>
2021-09-01 16:35:15 +0200

    Agregadas funciones para calcular edades
    Fueron agregadas las funciones para calcular la edad mayor, menor y el promedio de las edades de los integrantes familiares.

--- Tarea Clase 6 JS.js
+++ Tarea Clase 6 JS.js
@@ -7,27 +7,76 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 */
 
 const $botonCalculoIntegrantesFamiliares = document.querySelector('#botonCalculoIntegrantes');
+const $botonCalculoEdadesIntegrantes = document.querySelector('#botonCalculoEdadesIntegrantes');
+let arrayEdadesIntegrantesTotales;
 
-$botonCalculoIntegrantesFamiliares.onclick = function (){
+function crearCasillerosEdades(cantidadDeIntegrantesFamiliares) {
     const $cantidadIntegrantesTotales = Number(document.querySelector('#cantidadIntegrantesFamiliares').value);
-    const $edadesIntegrantesFamiliares = document.querySelector('#edadesIntegrantesFamiliares');
-    
+    const $formularioEdadesIntegrantesFamiliares = document.querySelector('#formularioEdadesIntegrantesFamiliares');
+
     for (let i = 0 ; i < $cantidadIntegrantesTotales ; i++) {
+        const $nuevoElementoDiv = document.createElement('div');
+        $formularioEdadesIntegrantesFamiliares.appendChild($nuevoElementoDiv);
+        
         const $labelIntegrante = document.createElement('label');
         $labelIntegrante.textContent = `Edad Integrante Familiar ${i+1} `;
-        $edadesIntegrantesFamiliares.appendChild($labelIntegrante);
-        
+        $nuevoElementoDiv.appendChild($labelIntegrante);
+
         const $casillaEdad = document.createElement('input');
         $casillaEdad.type = 'text';
         $casillaEdad.placeholder = 'Ingresa la edad de un integrante';
-        $casillaEdad.class = 'edadIntegrantes';
-        $edadesIntegrantesFamiliares.appendChild($casillaEdad);
-        
-    }
+        $casillaEdad.id = 'edadIntegrantesFamiliares';
+        $casillaEdad.style.width = '15%';
+        $nuevoElementoDiv.appendChild($casillaEdad);
 
-    return false;
+        const $botonCalculoEdades = document.createElement('button');
+        $botonCalculoEdades.id = 'botonCalcularEdades'
+    }
 }
 
+function crearArrayEdades() {
+    const $edadIntegrantesFamiliares = document.querySelectorAll('#edadIntegrantesFamiliares');
+    const arrayEdadesIntegrantes = [];
 
+    for ( let m = 0; m < $edadIntegrantesFamiliares.length ; m++) {
+        arrayEdadesIntegrantes.push(Number($edadIntegrantesFamiliares[m].value));
+    }
+    return arrayEdadesIntegrantesTotales = arrayEdadesIntegrantes;
+} 
+
+
+function calcularEdadMayor(edadesIntegrantes) {
+    const $edadMayorIntegrantes = document.querySelector('#edadMayorIntegrantes');
+    return $edadMayorIntegrantes.value = `La edad mayor de los integrantes es ${Math.max(...arrayEdadesIntegrantesTotales)}`;
+}
+
+function calcularEdadMenor(edadesIntegrantes) {
+    const $edadMenorIntegrantes = document.querySelector('#edadMenorIntegrantes');
+    return $edadMenorIntegrantes.value = `La edad menor de los integrantes es ${Math.min(...arrayEdadesIntegrantesTotales)}`;
+}
+
+function calcularPromedioEdades(edadesIntegrantes) {  
+    const $promedioEdadIntegrantes = document.querySelector('#promedioEdadIntegrantes');
+    
+    let totalEdades = 0;
+    
+    for (let z = 0 ; z < arrayEdadesIntegrantesTotales.length; z++ ) {
+        totalEdades = totalEdades + arrayEdadesIntegrantesTotales[z];
+    }
+    
+    let promedioTotalEdades = totalEdades / arrayEdadesIntegrantesTotales.length;
+    return $promedioEdadIntegrantes.value = `El promedio de la edad de los integrantes es ${promedioTotalEdades}`; 
+}
+
+$botonCalculoIntegrantesFamiliares.onclick = function (){ 
+     return crearCasillerosEdades();
+}
+
+$botonCalculoEdadesIntegrantes.onclick = function () {
+    crearArrayEdades();
+    calcularEdadMayor();
+    calcularEdadMenor();
+    calcularPromedioEdades();
+}
 
 
